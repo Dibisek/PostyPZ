532cc016be777762978807fd4fc696a9f019b746
KarelQ <123106509+KarelQ@users.noreply.github.com>
2025-01-25 04:40:29 +0100

    SCRUM-48 added bookmark

---
+++ public/views/bookmarks.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include_once __DIR__ . '/shared/head.php' ?>
+        
+        <link rel="stylesheet" type="text/css" href="public/css/style.css">
+        <link rel="stylesheet" type="text/css" href="public/css/mainpage.css">
+        <title>Bookmarks</title>
+    </head>
+
+    <body>
+        <div class="content">
+            <?php include_once __DIR__ . '/shared/leftbar.php' ?>
+            <section class="posts-feed">
+
+                <?php include_once __DIR__ . '/shared/display-posts.php' ?>
+            
+            </section>
+            <?php include_once __DIR__ . '/shared/rightbar.php' ?> 
+        </div>
+    </body>
